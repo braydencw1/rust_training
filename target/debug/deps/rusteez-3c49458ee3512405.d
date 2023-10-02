cc5842f226dfaf0475f3080dee52db88c38d111e
braydencw1 <braydencw1@gmail.com>
2023-10-01 23:43:13 -0500

    target

--- target/debug/deps/rusteez-3c49458ee3512405.d
+++
@@ -1,5 +0,0 @@
-/home/brayden/github/rust_training/target/debug/deps/rusteez-3c49458ee3512405: src/main.rs
-
-/home/brayden/github/rust_training/target/debug/deps/rusteez-3c49458ee3512405.d: src/main.rs
-
-src/main.rs:
--- target/debug/rusteez.d
+++
@@ -1,1 +0,0 @@
-/home/brayden/github/rust_training/target/debug/rusteez: /home/brayden/github/rust_training/src/main.rs
